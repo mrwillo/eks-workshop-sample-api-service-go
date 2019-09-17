02f7ae4adf8d35a4ea5de8d5e2c342f94c0945db
Duy Tran <tranduy86los@gmail.com>
2019-09-17 15:20:46 +0700

    update no1 to trigger build

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World, duy updated"}
+		res := &response{Message: "Hello World, duy updated, no 1"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
